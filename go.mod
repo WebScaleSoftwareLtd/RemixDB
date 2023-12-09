001d434967d26c464fb16f2cf9c226b3fcafd330
Astrid Gealer <astrid@gealer.email>
2023-12-09 17:05:08 +0000

    map the routes into go

--- go.mod
+++ go.mod
@@ -6,10 +6,14 @@ require (
 	github.com/davecgh/go-spew v1.1.1
 	github.com/fasthttp/router v1.4.22
 	github.com/fasthttp/websocket v1.5.7
+	github.com/gabriel-vasile/mimetype v1.4.3
+	github.com/google/uuid v1.4.0
 	github.com/iancoleman/strcase v0.3.0
 	github.com/jimeh/go-golden v0.1.0
 	github.com/juju/fslock v0.0.0-20160525022230-4d5c94c67b4b
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/mattn/go-ieproxy v0.0.11
+	github.com/otiai10/copy v1.14.0
 	github.com/stretchr/testify v1.8.1
 	github.com/valyala/fasthttp v1.51.0
 	golang.org/x/net v0.19.0
@@ -17,21 +21,15 @@ require (
 )
 
 require (
-	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-)
-
-require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/google/uuid v1.4.0
 	github.com/klauspost/compress v1.17.3 // indirect
-	github.com/mattn/go-ieproxy v0.0.11
-	github.com/otiai10/copy v1.14.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- webserver/router.go
+++ webserver/router.go
@@ -4,10 +4,85 @@
 package webserver
 
 import (
+	"io/fs"
+	"mime"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path/filepath"
 
+	"github.com/gabriel-vasile/mimetype"
 	"github.com/julienschmidt/httprouter"
+	"remixdb.io/frontend"
 )
+
+func mapFrontendStaticFiles(r *httprouter.Router, dist fs.FS) {
+	// Scope to the dist directory.
+	var err error
+	dist, err = fs.Sub(dist, "dist")
+	if err != nil {
+		panic(err)
+	}
+
+	// Map the static files to routes.
+	var processDir func(cursor fs.FS, routePrefix string)
+	processDir = func(cursor fs.FS, routePrefix string) {
+		dir, err := fs.ReadDir(cursor, "."+routePrefix)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, file := range dir {
+			// If route prefix is /, ignore both index.html and MAKE_GO_NOT_ERROR.
+			name := file.Name()
+			if routePrefix == "/" && (name == "index.html" || name == "MAKE_GO_NOT_ERROR") {
+				// We do not want to handle these files here.
+				continue
+			}
+
+			// Handle if this is a directory.
+			if file.IsDir() {
+				// Recurse into the directory.
+				processDir(cursor, routePrefix+name+"/")
+				continue
+			}
+
+			// Read the file from the filesystem.
+			b, err := fs.ReadFile(cursor, routePrefix+name)
+			if err != nil {
+				panic(err)
+			}
+
+			// Get the mime type.
+			mimeObj := mimetype.Detect(b)
+			mimeS := mimeObj.String()
+			if mimeS == "application/octet-stream" {
+				// Try and guess the mime type from the file extension.
+				mimeS = mime.TypeByExtension(filepath.Ext(name))
+				if mimeS == "" {
+					// Default to application/octet-stream.
+					mimeS = "application/octet-stream"
+				}
+			}
+
+			// Map the file to the route.
+			r.GET(routePrefix+name, func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+				w.Header().Set("Content-Type", mimeS+"; charset=utf-8")
+				w.Header().Set("Cache-Control", "public, max-age=31536000")
+				_, _ = w.Write(b)
+			})
+		}
+	}
+	processDir(dist, "/")
+}
+
+func mapFrontendIndexHtml(r *httprouter.Router, route string, indexHtml []byte) {
+	r.GET(route, func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		_, _ = w.Write(indexHtml)
+	})
+}
 
 func (w *WebServer) generateHttpRoutes(containsRpcRoutes bool) http.Handler {
 	r := httprouter.New()
@@ -22,6 +97,34 @@ func (w *WebServer) generateHttpRoutes(containsRpcRoutes bool) http.Handler {
 		}
 		r.POST("/rpc/:method", rpcHandler)
 		r.GET("/rpc", rpcHandler)
+	}
+
+	frontendHost := os.Getenv("REMIXDB_DEV_FRONTEND_HOST")
+	if frontendHost == "" {
+		// Load in the index.html file.
+		indexHtml, err := frontend.Dist.ReadFile("dist/index.html")
+		if err != nil {
+			indexHtml = []byte("RemixDB frontend not compiled into this binary!")
+		}
+
+		// Map the static files to routes.
+		mapFrontendStaticFiles(r, frontend.Dist)
+
+		// Serve the index.html file on the frontend routes.
+		for _, route := range frontend.Routes {
+			mapFrontendIndexHtml(r, route, indexHtml)
+		}
+	} else {
+		// Reverse proxy the frontend to the REMIXDB_DEV_FRONTEND_HOST if there is no route.
+		uri, err := url.Parse(frontendHost)
+		if err != nil {
+			// Set the URL scheme to http.
+			uri, err = url.Parse("http://" + frontendHost)
+		}
+		if err != nil {
+			panic(err)
+		}
+		r.NotFound = httputil.NewSingleHostReverseProxy(uri)
 	}
 
 	return r
